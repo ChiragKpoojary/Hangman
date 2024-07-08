70e8f2b23c3d466a5788aa13c40fab1fb5b45618
Chirag K Poojary <chiragkpoojary30@gmail.com>
2024-07-08 14:47:09 +0530

    none

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState,useCallback } from "react";
 
 const keys = [
-  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-  ["Z", "X", "C", "V", "B", "N", "M"]
+  ["Q", "W", "E", "R", "T", "Y", "U", "I",],
+  ["A", "S", "D", "F", "G", "H", "J", "K", "O"],
+  ["Z", "X", "C", "V", "B", "N", "M","L" ,"P"]
 ];
 
 interface KeyboardProps {
